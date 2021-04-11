d2927e863dafb0d340484ce634e54cda76158bed
Katherine Sang <katsang18@gmail.com>
2021-04-11 08:31:47 -0400

    some housecleaning, added model files and cleaned up all the original preprocessing stuff

--- preprocessing/download_miyazaki.py
+++ preprocessing/download_miyazaki.py
@@ -1,33 +1,37 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Jul 12 11:02:06 2020
-@author: OHyic
-"""
+
 #Import libraries
 from GoogleImageScrapper import GoogleImageScraper
 import os
 
-# #Define file path
-# # webdriver_path = os.path.normpath(os.getcwd()+"\\webdriver\\chromedriver.exe")
-# webdriver_path = "/usr/local/bin/chromedriver"
-# image_path = "/Users/ksang/Documents/cs_2021/cs1430/ganilla_test/night"
-# print(webdriver_path)
-# print(image_path)
+"""
+Created on Sun Jul 12 11:02:06 2020
+@author: OHyic
+@modified / debugged: Minna + Katherine
+"""
+def scrape_google():
+    #Define file path
+    # webdriver_path = os.path.normpath(os.getcwd()+"\\webdriver\\chromedriver.exe")
+    webdriver_path = "/usr/local/bin/chromedriver"
+    image_path = "/Users/ksang/Documents/cs_2021/cs1430/ganilla_test/night"
+    print(webdriver_path)
+    print(image_path)
 
-# #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
-# search_keys= ["ghibli rainy wallpaper"]
+    #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
+    search_keys= ["ghibli rainy wallpaper"]
 
-# #Parameters
-# number_of_images = 50
-# headless = False
-# min_resolution=(0,0)
-# max_resolution=(3000,3000)
+    #Parameters
+    number_of_images = 50
+    headless = False
+    min_resolution=(0,0)
+    max_resolution=(3000,3000)
 
-# #Main program
-# for search_key in search_keys:
-#     image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
-#     image_urls = image_scrapper.find_image_urls()
-#     image_scrapper.save_images(image_urls)
+    #Main program
+    for search_key in search_keys:
+        image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
+        image_urls = image_scrapper.find_image_urls()
+        image_scrapper.save_images(image_urls)
+
 
 """
 @author: Minna + Katherine
@@ -41,4 +45,8 @@ def rename_files():
         count +=1
 
 
-rename_files()
+""""
+Execute desired functions here
+""""
+# rename_files()
+# scrape_google()
