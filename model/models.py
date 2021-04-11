b8be41b941999748775c74caff03d0b57e7ce96f
Katherine Sang <katsang18@gmail.com>
2021-04-11 10:45:44 -0400

    added discriminator templating and notes

--- model/models.py
+++ model/models.py
@@ -19,6 +19,7 @@ class GANILLA(tf.keras.Model):
         self.num_classes = hp.num_classes
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=hp.learning_rate)
 
+        # TODO: Instantiate the 2 pairs of Discrim/Gen
         self.architecture = [
              
         ]
@@ -42,7 +43,7 @@ class GANILLA(tf.keras.Model):
         return loss(labels, predictions)
 
 """
-The loss model, containing modified RESNET blocks.
+The Generator model, containing modified RESNET blocks.
 """
 class Generator(tf.keras.Model):
     def __init__(self):
@@ -53,18 +54,21 @@ class Generator(tf.keras.Model):
         self.block1 = [
             Conv2D(filters=64, kernel_size=7, strides=1, padding="same", name="block1_conv1"),
             InstanceNormalization(axis=-1),
-            Activation('relu') # seems to go after normalization not CONV2d, need to fact check
+            Activation("relu") # seems to go after normalization not CONV2D, need to fact check if this is legit
             MaxPool2D(2, name="block1_conv1"),   
         ]
 
         #TODO: Upsampling
         self.upsample = []
+        # use  tf.image.resize(image, size=[5,7], method="nearest") for upsampling, unsure about shape
 
     def call(self, x):
         """ Passes the image through the network. """
 
         x = self.block1(x)
-        x = self.resnet(x)
+
+        #TODO: a guess for the 4 downsampling blocks: call resnet on on 64, 128, 256 , 512
+        x = self.resnet(x, 64)
         x = self.upsample(x)
 
         return x
@@ -83,16 +87,19 @@ class Generator(tf.keras.Model):
         """ Returns the output of a single resnet block """
         #TODO: Not sure if for each resent block, the filter size decreases so I have it as a variable rn incase it halves.
         # RESNET18 does that but TBH the architecture is a little different from what I see in the paper.
+
+        #NOTE: Not totally sure of the strides, this line is a bit confusing: 
+        # "We halve feature map size in each layer except Layer-I using convolutions with stride of 2."
         KERNEL_INIT = RandomNormal(stddev=0.02) 
         KERNEL_SIZE = 3
         
         mod_resnet = [
-            Conv2D(filters=filter_size, kernel_size=KERNEL_SIZE, strides=(2,2), padding='same', 
+            Conv2D(filters=filter_size, kernel_size=KERNEL_SIZE, strides=(2,2), padding="same", 
                 kernel_initializer=KERNEL_INIT, name="conv1"),
             InstanceNormalization(axis=-1),
-            Activation('relu'),
+            Activation("relu"),
             MaxPool2d(strides=2),
-            Conv2D(filters=filter_size, kernel_size=KERNEL_SIZE, strides=(2,2), padding='same', 
+            Conv2D(filters=filter_size, kernel_size=KERNEL_SIZE, strides=(2,2), padding="same", 
                 kernel_initializer=KERNEL_INIT, name="conv1"),
             InstanceNormalization(axis=-1),
         ]
@@ -101,38 +108,83 @@ class Generator(tf.keras.Model):
         result = Concatenate()([output, inputs])
 
         final_layer = [
-            Conv2D(filters=filter_size, kernel_size=3, strides(1,1), padding='same', activation='relu', kernel_initializer=KERNEL_INIT)
+            Conv2D(filters=filter_size, kernel_size=3, strides(1,1), padding="same", activation="relu", kernel_initializer=KERNEL_INIT)
         ]
 
         return result
 
 
-
         # vanilla_resnet = [
-        #     Conv2D(filters=64, kernel_size=7, strides=(2,2), padding='same', 
-        #         kernel_initializer='random_normal', activation = 'relu', name="conv1"),
+        #     Conv2D(filters=64, kernel_size=7, strides=(2,2), padding="same", 
+        #         kernel_initializer="random_normal", activation = "relu", name="conv1"),
         #     MaxPool2D(3, stride=2),
             
-        #     Conv2D(filters=64, kernel_size=3, strides=(2,2), padding='same', 
-        #         kernel_initializer='random_normal', activation = 'relu', name="conv2_x"),
-        #     Conv2D(filters=64, kernel_size=3, strides=(2,2), padding='same', 
-        #         kernel_initializer='random_normal', activation = 'relu', name="conv2_x"),
+        #     Conv2D(filters=64, kernel_size=3, strides=(2,2), padding="same", 
+        #         kernel_initializer="random_normal", activation = "relu", name="conv2_x"),
+        #     Conv2D(filters=64, kernel_size=3, strides=(2,2), padding="same", 
+        #         kernel_initializer="random_normal", activation = "relu", name="conv2_x"),
 
-        #     Conv2D(filters=128, kernel_size=3, strides=(2,2), padding='same', 
-        #         kernel_initializer='random_normal', activation = 'relu', name="conv3_x"),
-        #     Conv2D(filters=128, kernel_size=3, strides=(2,2), padding='same', 
-        #         kernel_initializer='random_normal', activation = 'relu', name="conv3_x"),
+        #     Conv2D(filters=128, kernel_size=3, strides=(2,2), padding="same", 
+        #         kernel_initializer="random_normal", activation = "relu", name="conv3_x"),
+        #     Conv2D(filters=128, kernel_size=3, strides=(2,2), padding="same", 
+        #         kernel_initializer="random_normal", activation = "relu", name="conv3_x"),
 
-        #     Conv2D(filters=256, kernel_size=3, strides=(2,2), padding='same', 
-        #         kernel_initializer='random_normal', activation = 'relu', name="conv4_x"),
-        #     Conv2D(filters=256, kernel_size=3, strides=(2,2), padding='same', 
-        #         kernel_initializer='random_normal', activation = 'relu', name="conv4_x"),
+        #     Conv2D(filters=256, kernel_size=3, strides=(2,2), padding="same", 
+        #         kernel_initializer="random_normal", activation = "relu", name="conv4_x"),
+        #     Conv2D(filters=256, kernel_size=3, strides=(2,2), padding="same", 
+        #         kernel_initializer="random_normal", activation = "relu", name="conv4_x"),
             
-        #     Conv2D(filters=512, kernel_size=3, strides=(2,2), padding='same', 
-        #         kernel_initializer='random_normal', activation = 'relu', name="conv4_x"),
-        #     Conv2D(filters=512, kernel_size=3, strides=(2,2), padding='same', 
-        #         kernel_initializer='random_normal', activation = 'relu', name="conv4_x"),
-        #     AveragePooling2D(pool_size=(2,2), padding='same'),
+        #     Conv2D(filters=512, kernel_size=3, strides=(2,2), padding="same", 
+        #         kernel_initializer="random_normal", activation = "relu", name="conv4_x"),
+        #     Conv2D(filters=512, kernel_size=3, strides=(2,2), padding="same", 
+        #         kernel_initializer="random_normal", activation = "relu", name="conv4_x"),
+        #     AveragePooling2D(pool_size=(2,2), padding="same"),
         #     Dense(1000, activation=softmax) 
         # ]
 
+
+
+"""
+The Discriminator model, leveraging PatchGAN architecture.
+Determines if the given image is generated or real.
+
+Nice explanation of PatchGAN first bit: https://sahiltinky94.medium.com/understanding-patchgan-9f3c8380c207
+Note that this explanation has only one layer in each block. This may be worthwhile simplification to improve runtime.
+
+Tutorial: https://machinelearningmastery.com/how-to-implement-pix2pix-gan-models-from-scratch-with-keras/
+"""
+class Discriminator(tf.keras.Model):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=hp.learning_rate)
+
+        KERNEL_SIZE = 3
+        
+        #TODO: Model, this has not been started really.
+        self.layers = [
+            Conv2D(filters=64, kernel_size=KERNEL_SIZE, strides=1, padding="same", name="block1_conv1", activation="relu"),
+            Conv2D(filters=64, kernel_size=KERNEL_SIZE, strides=1, padding="same", name="block1_conv2", activation="relu"),
+
+            Conv2D(filters=128, kernel_size=KERNEL_SIZE, strides=1, padding="same", name="block2_conv1", activation="relu"),
+            Conv2D(filters=128, kernel_size=KERNEL_SIZE, strides=1, padding="same", name="block2_conv2", activation="relu"),
+
+            Conv2D(filters=256, kernel_size=KERNEL_SIZE, strides=1, padding="same", name="block3_conv1", activation="relu"),
+            Conv2D(filters=256, kernel_size=KERNEL_SIZE, strides=1, padding="same", name="block3_conv2", activation="relu"),
+
+        ]
+
+
+    def call(self, x):
+        x = self.layers(x)
+        return x
+
+    @staticmethod
+    def loss_fn(labels, predictions):
+        """ Loss function for model. """
+
+        # TODO: Select a loss function for your network (see the documentation
+        #       for tf.keras.losses)
+
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+        return loss(labels, predictions)
