a4228f15c79fb1baa859fdff47f5f43860ecc3fd
Katherine Sang <katsang18@gmail.com>
2021-04-13 19:43:48 -0400

    clean up preprocessing

--- model/preprocess.py
+++ model/preprocess.py
@@ -60,16 +60,12 @@ class Datasets():
             img = Image.open(file_path)
             
             img = img.resize((hp.img_size, hp.img_size))
-            
             img = np.array(img, dtype=np.float32)
             
-
-            print(img.shape, file_path)
-
             img /= 255.
 
-
-            data_sample[i] = img
+            if (len(img.shape) == 3):
+                data_sample[i] = img
 
         # Calculate the pixel-wise mean and standard deviation
         # of the images in data_sample and store them in
@@ -106,7 +102,6 @@ class Datasets():
         """ Preprocess function for ImageDataGenerator. """
         img = img / 255.
         # img = self.standardize(img)
-        img = tf.image.resize(img, [256, 256], preserve_aspect_ratio=False,antialias=False, name=None)
         return img
 
     def get_data(self, path, shuffle, augment):
@@ -137,7 +132,7 @@ class Datasets():
             data_gen = tf.keras.preprocessing.image.ImageDataGenerator(
                 preprocessing_function=self.preprocess_fn, 
                 horizontal_flip=True, 
-                zoom_range=[.5, .8],
+                zoom_range=[.5, 1],
                 fill_mode='nearest')
             # ============================================================
         else:
@@ -149,31 +144,11 @@ class Datasets():
         img_size = hp.img_size
 
         # Form image data generator from directory structure
-        # data_gen = data_gen.flow_from_directory(
-        #     path,
-        #     target_size=(img_size, img_size),
-        #     save_to_dir=os.path.join(self.data_path, "preprocess/"),
-        #     batch_size=hp.batch_size,
-        #     shuffle=shuffle)
-        file_list=[]
-        for root, _, files in os.walk(os.path.join(self.data_path, "train/")):
-            for name in files:
-                if name.endswith(".jpg"):
-                    file_list.append(os.path.join(root, name))
-
-        # Import images
-        images = []
-        for i, file_path in enumerate(file_list):
-            img = Image.open(file_path)
-            img = np.array(img)
-            img = self.preprocess_fn(img)
-            img = np.expand_dims(img, axis=0)
-            # img  = tf.keras.preprocessing.image.img_to_array(img, data_format=None, dtype=None)
-            images.append(img)
-        # print(np.array(images).shape)
-            for x, val in zip(data_gen.flow(img, save_to_dir=os.path.join(self.data_path, "train/preprocess/"), save_prefix='aug', save_format='png'), range(10)):
-                pass
-
-
-        
+        data_gen = data_gen.flow_from_directory(
+            path,
+            target_size=(img_size, img_size),
+            save_to_dir=os.path.join(self.data_path, "preprocess/"),
+            batch_size=hp.batch_size,
+            shuffle=shuffle)
+  
         return data_gen
